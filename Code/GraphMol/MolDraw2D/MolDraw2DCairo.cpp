2779068e05a0aef919da8bd06811480d4818d020
Martijn <martijn.oldenhof@kuleuven.be>
2020-07-13 09:50:50 +0200

    add Bold Font option

--- Code/GraphMol/MolDraw2D/MolDraw2DCairo.cpp
+++ Code/GraphMol/MolDraw2D/MolDraw2DCairo.cpp
@@ -19,13 +19,26 @@ void MolDraw2DCairo::initDrawing() {
   PRECONDITION(dp_cr, "no draw context");
  // cairo_select_font_face(dp_cr, "sans", CAIRO_FONT_SLANT_NORMAL,
  //                        CAIRO_FONT_WEIGHT_NORMAL);
+   
   if(const char* env_p = std::getenv("RDKIT_FONTFACE")){
-    cairo_select_font_face(dp_cr, env_p, CAIRO_FONT_SLANT_NORMAL,
+    if(const char* env_bold = std::getenv("RDKIT_FONTBOLD")){
+       cairo_select_font_face(dp_cr, env_p, CAIRO_FONT_SLANT_NORMAL,
+                         CAIRO_FONT_WEIGHT_BOLD);
+    }
+    else{
+       cairo_select_font_face(dp_cr, env_p, CAIRO_FONT_SLANT_NORMAL,
                          CAIRO_FONT_WEIGHT_NORMAL);
+    }
   }
   else{
-    cairo_select_font_face(dp_cr, "sans", CAIRO_FONT_SLANT_NORMAL,
+    if(const char* env_bold = std::getenv("RDKIT_FONTBOLD")){
+       cairo_select_font_face(dp_cr, "sans", CAIRO_FONT_SLANT_NORMAL,
+                         CAIRO_FONT_WEIGHT_BOLD);
+    }
+    else{
+       cairo_select_font_face(dp_cr, "sans", CAIRO_FONT_SLANT_NORMAL,
                          CAIRO_FONT_WEIGHT_NORMAL);
+    }
   }
   cairo_set_font_size(dp_cr, fontSize());
   cairo_set_line_cap(dp_cr, CAIRO_LINE_CAP_BUTT);
